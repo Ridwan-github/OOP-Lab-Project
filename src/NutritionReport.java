d97a4d3d49bbcf315f475b671fd27275562142c2
Ridwan <ridwanraish2003@gmail.com>
2025-04-03 02:29:01 +0600

    Created NutritionReport Class

---
+++ src/NutritionReport.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class NutritionReport {
+    private User user;
+    private Map<String, Double> nutritionStats;
+    public NutritionReport(User user) {
+        this.user = user;
+        this.nutritionStats = new HashMap<>();
+    }
+
+    public void generateDailyStatistics() {
+        double totalCalories = 0;
+        double totalProtein = 0;
+        double totalCarbs = 0;
+        double totalFats = 0;
+
+        for (Meal meal : user.getConsumedMeals()) {
+            totalCalories += meal.calculateTotalCalories();
+        }
+
+        nutritionStats.put("calories", totalCalories);
+        nutritionStats.put("calorieNeeds", user.calculateDailyCalorieNeeds());
+        nutritionStats.put("calorieDeficit", user.calculateDailyCalorieNeeds() - totalCalories);
+    }
+
+    public void display() {
+        System.out.println("=== Nutrition Report for " + user.getName() + " ===");
+        System.out.println("Daily Calorie Needs: " + nutritionStats.get("calorieNeeds"));
+        System.out.println("Consumed Calories: " + nutritionStats.get("calories"));
+        System.out.println("Calorie Deficit/Surplus: " + nutritionStats.get("calorieDeficit"));
+        System.out.println("Protein: " + nutritionStats.get("protein") + "g");
+        System.out.println("Carbs: " + nutritionStats.get("carbs") + "g");
+        System.out.println("Fats: " + nutritionStats.get("fats") + "g");
+    }
+}
