f8ab68f99ae098630b64c08bb1225795548f2d25
Ridwan <ridwanraish2003@gmail.com>
2025-04-03 02:51:41 +0600

    Implemented the core system class (NutritionSystem)

---
+++ src/NutritionSystem.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NutritionSystem {
+    private List<User> users;
+    private FoodList foodlist;
+    private Map<User, List<HealthGoal>> userGoals;
+
+    public NutritionSystem() {
+        this.users = new ArrayList<>();
+        this.foodlist = new FoodList();
+        this.userGoals = new HashMap<>();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+        userGoals.put(user, new ArrayList<>());
+    }
+
+    public void addHealthGoal(User user, HealthGoal goal) {
+        if (userGoals.containsKey(user)) {
+            userGoals.get(user).add(goal);
+        }
+    }
+
+    public FoodList getFoodList() {
+        return foodlist;
+    }
+
+    public NutritionReport generateDailyReport(User user) {
+        NutritionReport report = new NutritionReport(user);
+        report.generateDailyStatistics();
+        return report;
+    }
+}
