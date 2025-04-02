0e36efa8536b390c892fd64d1a4c4827e8b0d417
Ridwan <ridwanraish2003@gmail.com>
2025-04-03 01:06:30 +0600

    Added Food class

---
+++ src/Food.java
@@ -0,0 +1,16 @@
+public class Food {
+    private String name;
+    private double calories;
+
+    public Food(String name, double calories) {
+        this.name = name;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getCalories() {
+        return calories;
+    }
+}
