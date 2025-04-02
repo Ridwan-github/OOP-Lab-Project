49b794444d4d4c497227ba52e469b025a20b1b1a
Ridwan <ridwanraish2003@gmail.com>
2025-04-03 01:21:53 +0600

    Added Meal class

---
+++ src/Meal.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Meal {
+    private String name;
+    private List<Food> foodItems;
+
+    public Meal(String name, List<Food> foodItems) {
+        this.name = name;
+        this.foodItems = foodItems;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public List<Food> getFoodItems() {
+        return foodItems;
+    }
+
+    public double calculateTotalCalories(){
+        double totalCalories = 0;
+        for (Food food : foodItems) {
+            totalCalories += food.getCalories();
+        }
+        return totalCalories;
+    }
+}
