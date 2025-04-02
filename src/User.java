03e2100795db9a7c35fea30a1caa649a58a09f95
Ridwan <ridwanraish2003@gmail.com>
2025-04-03 00:43:59 +0600

    User class extended

--- src/User.java
+++ src/User.java
@@ -3,4 +3,30 @@ public class User {
     private double weight; // in kg
     private double height; // in cm
     private int age;
+
+    public User(String name, double weight, double height, int age) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.age = age;
+    }
+
+    public double calculateBMI() {
+        return weight / Math.pow(height/100, 2);
+    }
+    public String getName() {
+        return name;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
