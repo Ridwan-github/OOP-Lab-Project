7c5def28a067a498ab7c8beed419b5f0edc78621
Ridwan <ridwanraish2003@gmail.com>
2025-04-03 00:48:43 +0600

    User class updated + added gender enum

--- src/User.java
+++ src/User.java
@@ -3,6 +3,7 @@ public class User {
     private double weight; // in kg
     private double height; // in cm
     private int age;
+    private Gender gender;
 
     public User(String name, double weight, double height, int age) {
         this.name = name;
@@ -14,6 +15,15 @@ public class User {
     public double calculateBMI() {
         return weight / Math.pow(height/100, 2);
     }
+    public double calculateBMR() {
+        if (gender == Gender.MALE) {
+            return 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+        } else {
+            return 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+        }
+    }
+
+
     public String getName() {
         return name;
     }
